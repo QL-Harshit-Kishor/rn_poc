2c4e4b34d248ae4b7764d4421edd6290ecc51615
Harshit Kishor <harshit.kishor@quokkalabs.com>
2024-04-28 22:04:09 +0530

    [INIT]: inmitialize folder structure

---
+++ src/assets/index.tsx
@@ -0,0 +1,35 @@
+// Splash Svg Component
+import ThemeSvg from './svg/change_theme.svg';
+
+//! Export all Fonts  from here
+export const FontConst = {
+  Satoshi: {
+    regular: 'Satoshi-Regular',
+    bold: 'Satoshi-Bold',
+    light: 'Satoshi-Light',
+    medium: 'Satoshi-Medium',
+  },
+};
+
+//! Export all Image path from here
+export const ImageConst = {
+  DEFAULT: require('./images/default.png'),
+};
+
+//! Export all Lottie path from here
+export const LottieConst = {
+  ERROR: require('./lottie/error.json'),
+
+};
+
+//! Export all Svg Components from here
+export {
+    ThemeSvg
+};
+
+/* 
+
+For Svg use==>
+<LoginSvg height={50} width={40} />
+
+*/
